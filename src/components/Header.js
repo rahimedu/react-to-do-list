fc09de0f3835c23c94b6d7c673aa8b951f1fb3f5
rahimedu <edwin.duche82@gmail.com>
2022-09-09 23:42:17 -0400

    inith.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,4 +2,20 @@ import React from 'react';
 
 const Header = ({ handleToggleDarkMode }) => {
 	return (
-	
+		<div className='header'>
+			<h1>My.ToDo</h1>
+			<button
+				onClick={() =>
+					handleToggleDarkMode(
+						(previousDarkMode) => !previousDarkMode
+					)
+				}
+				className='save'
+			>
+				Toggle
+			</button>
+		</div>
+	);
+};
+
+export default Header;
