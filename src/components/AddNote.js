780c5582060bbbabd6aeb0fe257e748d0f3e7d66
rahimedu <edwin.duche82@gmail.com>
2022-09-09 23:18:53 -0400

    doneAddnotejs

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -9,9 +9,8 @@ const AddNote = ({ handleAddNote }) => {
 	const handleChange = (event) => {
 		if (characterLimit - event.target.value.length >= 0) {
 			setNoteText(event.target.value);
-		}
-	};
 
+			
 	const handleSaveClick = () => {
 		if (noteText.trim().length > 0) {
 			handleAddNote(noteText);
