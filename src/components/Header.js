db2a7c511410b39082086acb827c7832779f7dc3
rahimedu <edwin.duche82@gmail.com>
2022-09-09 23:41:12 -0400

    doneHeader

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,4 @@ import React from 'react';
 
 const Header = ({ handleToggleDarkMode }) => {
 	return (
-		<div className='header'>
-			<h1>My.ToDo</h1>
-			<button
-				onClick={() =>
-					handleToggleDarkMode(
-						(previousDarkMode) => !previousDarkMode
-					)
-				}
-				className='save'
-			>
-				Toggle
-			</button>
-		</div>
-	);
-};
-
-export default Header;
+	
