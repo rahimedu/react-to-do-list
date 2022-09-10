89f8a5cd246b88e4d5e309468c2725bbbc87dfee
rahimedu <edwin.duche82@gmail.com>
2022-09-09 23:11:19 -0400

    done

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import React from 'react'
 
 
+const AddNote = ({ handleAddNote }) => {
+	const [noteText, setNoteText] = useState('');
+	const characterLimit = 200;
+
 	const handleChange = (event) => {
 		if (characterLimit - event.target.value.length >= 0) {
 			setNoteText(event.target.value);
